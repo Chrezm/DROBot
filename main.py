7a38b2840d021201eddb26425e853f33e369cc7d
Chrezm <TheChrezm@gmail.com>
2021-04-30 15:43:52 -0400

    Added $rpactive

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ class test_discord():
         816054356091994164
     }
 
+    rp_active_role_name = 'epic role'
+    rp_active_role_id = 816054356091994164
+
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
@@ -39,12 +43,37 @@ def _validate_command(ctx: commands.Context) -> bool:
 
     return False
 
+
 @bot.command(name='ping')
 async def ping(ctx: commands.Context):
     if not _validate_command(ctx):
         return
 
-    await ctx.channel.send("pong")
+    await ctx.channel.send('Pong.')
+
+
+@bot.command(name='rpactive')
+async def rpactive(ctx: commands.Context):
+    if not _validate_command(ctx):
+        return
+
+    rp_active_role = discord.utils.get(ctx.message.guild.roles,
+                                       name=guild_details.rp_active_role_name)
+
+    user = ctx.author
+    has_rp_active = False
+
+    for role in user.roles:
+        if role.id == guild_details.rp_active_role_id:
+            has_rp_active = True
+
+    if has_rp_active:
+        await user.remove_roles(rp_active_role)
+        await ctx.send(f'Removed role **{guild_details.rp_active_role_name}**.')
+    else:
+        await user.add_roles(rp_active_role)
+        await ctx.send(f'Added role **{guild_details.rp_active_role_name}**.')
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -52,6 +81,7 @@ async def on_command_error(ctx, error):
         # No need to do anything fancy here
         return
     raise error
+
 
 @bot.event
 async def on_message(message):
@@ -64,10 +94,8 @@ async def on_message(message):
             prefix=guild_details.relaying_prefix,
             suffix=guild_details.relaying_suffix)
 
-    try:
-        await bot.process_commands(message)
-    except Exception as exc:
-        breakpoint()
+    await bot.process_commands(message)
+
 
 def get_channel(name):
     channel = discord.utils.get(bot.get_all_channels(), name=name)
